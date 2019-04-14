57e32f931bfa04f0c793ec7db7246a6c3f75f72a
horacechaunce <horacechaunce@sina.com>
2019-04-14 22:33:57 +0800

    提交修改

---
+++ JavaArray/src/com/shihong/test/array/ArrayConparisionSize.java
@@ -0,0 +1,65 @@
+package com.shihong.test.array;
+
+/**
+ * @author shihong on 2019/4/14.
+ */
+public class ArrayConparisionSize {
+
+    public static void main(String[] args) {
+        int[] arr = {-1, 94, 2, 45, 23, 43, 78, 6, 69};
+        //正序遍历 arr.fori
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+
+        }
+        System.out.println("------------华丽的分隔线--------------");
+        //逆序遍历 arr.forr
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.println(arr[i]);
+
+        }
+
+        //遍历获取最值 方法一，利用中间变量比较
+        int maxInt = getMaxInt(arr);
+        System.out.println("max :" + maxInt);
+        //遍历获取最值 方法二，利用数组角标做为中间变量
+        int minInt = getMinInt(arr);
+        System.out.println("min:" + minInt);
+    }
+
+    /**
+     * 遍历获取最值 方法一，利用中间变量比较
+     *
+     * @param arr
+     * @return
+     */
+    public static int getMaxInt(int[] arr) {
+        //定义一个中间变量用于比较大小
+        int temp = arr[0];
+        //这里 i  的值从1开始是因为定义中间变量用于比较时已经用了arr[0]
+        //在数组遍历比较的时候没有必要再比较一次自身
+        for (int i = 1; i < arr.length; i++) {
+            if (temp < arr[i]) {
+                temp = arr[i];
+            }
+        }
+        return temp;
+    }
+
+    /**
+     * 遍历获取最值 方法二，利用数组角标做为中间变量
+     *
+     * @param arr
+     * @return
+     */
+    public static int getMinInt(int[] arr) {
+        //定义一个中间变量，利用数组角标
+        int temp = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[temp] > arr[i]) {
+                temp = i;
+            }
+        }
+        return arr[temp];
+    }
+}
--- JavaArray/src/com/shihong/test/array/ArraySort.java
+++ JavaArray/src/com/shihong/test/array/ArraySort.java
@@ -1,9 +1,10 @@
 package com.shihong.test.array;
 
+
 /**
  * @author shihong on 2019/4/13.
  */
-public class TestArray {
+public class ArraySort {
 
     /**
      * 输出排序使用方法
@@ -27,24 +28,7 @@ public class TestArray {
      */
     public static void main(String[] args) {
         int[] arr = {-1, 94, 2, 45, 23, 43, 78, 6, 69};
-        //正序遍历 arr.fori
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
 
-        }
-        System.out.println("------------华丽的分隔线--------------");
-        //逆序遍历 arr.forr
-        for (int i = arr.length - 1; i >= 0; i--) {
-            System.out.println(arr[i]);
-
-        }
-
-        //遍历获取最值 方法一，利用中间变量比较
-        int maxInt = getMaxInt(arr);
-        System.out.println("max :" + maxInt);
-        //遍历获取最值 方法二，利用数组角标做为中间变量
-        int minInt = getMinInt(arr);
-        System.out.println("min:" + minInt);
 
         //选择排序
         outPut(arr);//排序前
@@ -53,51 +37,17 @@ public class TestArray {
 
         //冒泡排序一
         outPut(arr);
-        maoPaoSort_1(arr);
+        bubbleSort_1(arr);
         outPut(arr);
         //冒泡排序二
         outPut(arr);
-        maoPaoSort_2(arr);
+        bubbleSort_2(arr);
         outPut(arr);
 
 
     }
 
-    /**
-     * 遍历获取最值 方法一，利用中间变量比较
-     *
-     * @param arr
-     * @return
-     */
-    public static int getMaxInt(int[] arr) {
-        //定义一个中间变量用于比较大小
-        int temp = arr[0];
-        //这里 i  的值从1开始是因为定义中间变量用于比较时已经用了arr[0]
-        //在数组遍历比较的时候没有必要再比较一次自身
-        for (int i = 1; i < arr.length; i++) {
-            if (temp < arr[i]) {
-                temp = arr[i];
-            }
-        }
-        return temp;
-    }
 
-    /**
-     * 遍历获取最值 方法二，利用数组角标做为中间变量
-     *
-     * @param arr
-     * @return
-     */
-    public static int getMinInt(int[] arr) {
-        //定义一个中间变量，利用数组角标
-        int temp = 0;
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[temp] > arr[i]) {
-                temp = i;
-            }
-        }
-        return arr[temp];
-    }
 
     /**
      * 选择排序
@@ -126,7 +76,7 @@ public class TestArray {
      *
      * @param arr
      */
-    public static void maoPaoSort_1(int[] arr) {
+    public static void bubbleSort_1(int[] arr) {
         //长度-1因为最后一个数组元素没有必要跟自身比较
         for (int i = 0; i < arr.length - 1; i++) {
             // 内循环长度-1是为了防止下标越界，
@@ -146,7 +96,7 @@ public class TestArray {
      *
      * @param arr
      */
-    public static void maoPaoSort_2(int[] arr) {
+    public static void bubbleSort_2(int[] arr) {
 
         //i>0因为倒序循环时最后一个数组元素没有必要跟自身比较即arr[0]
         for (int i = arr.length - 1; i > 0; i--) {
