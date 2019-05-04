8a026c736772b46c7431680dbc6355fdd65b4c9c
horacechaunce <horacechaunce@sina.com>
2019-05-04 15:27:04 +0800

    单例设计模式

--- javaAll/src/com/shihong/test/object/Singleton.java
+++ javaAll/src/com/shihong/test/object/Singleton.java
@@ -5,7 +5,7 @@ package com.shihong.test.object;
  * 单例设计模式 饿汉式
  */
 /*public class Singleton {
-    private static Singleton single = new Singleton();
+    private static final Singleton single = new Singleton(); //加final防止被子类属性覆盖
 
     private Singleton() {}
 
@@ -14,7 +14,24 @@ package com.shihong.test.object;
     }
 }*/
 
-/**    
+/**
+ * //这种写法仍然使用JVM本身机制保证了线程安全问题；
+ *  由于 SingletonHolder 是私有的，除了 getInstance() 之外没有办法访问它，
+ *  因此它是懒汉式的；同时读取实例的时候不会进行同步，没有性能缺陷；也不依赖 JDK 版本。
+ * public class Singleton {
+ *
+     private static class SingletonHolder {
+         private static final Singleton INSTANCE = new Singleton();
+         }
+     private Singleton (){}
+
+     public static final Singleton getInstance() {
+        return SingletonHolder.INSTANCE;
+     }
+    }
+ */
+
+/**
  * 单例模式，懒汉式
  * 在开发实际使用中，使用饿汉式比较多。因为懒汉式存在线程问题
  */
@@ -24,9 +41,22 @@ public class Singleton {
     private Singleton() {
     }
 
-    public static Singleton getInstance() {
-        if (single == null) {
+  /*  //同步函数的方式
+    public static synchronized Singleton getInstance() {
+        if (single == null)
             single = new Singleton();
+
+        return single;
+    }*/
+
+    //同步代码块加锁的方式 安全高效
+    public static Singleton getInstanceBlock() {
+        if (single == null) {
+            synchronized (Singleton.class) { //同步线程
+                if (single == null) {
+                    single = new Singleton();
+                }
+            }
         }
         return single;
     }
