f6cdc5f078253fc62c8207391a6f291d77f05f54
horacechaunce <horacechaunce@sina.com>
2020-09-07 17:26:23 +0800

    面向对象

---
+++ javaAll/src/com/shihong/test/oop/ObjectOriented.java
@@ -0,0 +1,30 @@
+package com.shihong.test.oop;
+
+/**
+ * @author shihong on 2020/8/28.
+ *         面向对象
+ */
+public class ObjectOriented {
+
+	/* 面向对象的三大特征
+	a)	封装   (Encapsulation)
+    b)	继承   (Inheritance)
+    c)	多态   (Polymorphism)
+    d)  OOP: Object Oriented Programming 面向对象编程
+    e)  面向过程：procedure oriented programming
+
+
+    类(class)和对象(object)是面向对象的核心概念。
+    a)  类是对一类事物描述，是抽象的、概念上的定义
+    b) 对象是实际存在的该类事物的每个个体，因而也称实例(instance)。
+
+
+    属 性：对应类中的成员变量
+    行 为：对应类中的成员方法
+    Field = 属性 = 成员变量，
+    Method = (成员)方法 = 函数
+
+
+
+	*/
+}
